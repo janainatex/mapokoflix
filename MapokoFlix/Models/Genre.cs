32c41703ff10f006eb6a9d6e15ef811b02f309f7
MapokoFlix <janainajapa05@gmail.com>
2023-05-15 08:40:24 -0300

    Inserção de Anotação de Dados nas Classes de Modelo

--- MapokoFlix/Models/Genre.cs
+++ MapokoFlix/Models/Genre.cs
@@ -1,6 +1,12 @@
+using  System.ComponentModel.DataAnnotations;
+
 namespace MapokoFlix.Models;
     public class Genre
     {
+        [Key ] // É uma notação para chave primária 
         public byte Id { get; set; }
+
+        [Required] // Requerido - Not Null no banco ; Torna o nome obrigatório
+        [StringLength(30)]  //Tamanho máximo da propriedade 
         public string Name { get; set; }
     }
--- MapokoFlix/Models/Movie.cs
+++ MapokoFlix/Models/Movie.cs
@@ -1,15 +1,37 @@
-namespace MapokoFlix.Models;
+using  System.ComponentModel.DataAnnotations;
+using  System.ComponentModel.DataAnnotations.Schema;
 
     public class Movie
     {
+        [Key]
         public int id { get; set; }
+
+        [ Required]
+        [StringLength(100)]
         public string Title { get; set; }
+
+    
+         [ Required]
+        [StringLength(100)]
         public string OriginalTitle { get; set; }
+
+         [ Required]
+        [StringLength(8000)]
         public string Synopsis { get; set; }
+
+         [Required]
+        [Column(TypeName = "Year")]
         public Int16 MovieYear { get; set; }
+
+        [Required]
         public Int16 Duration { get; set; }
-        public byte AgeRating { get; set; }
+
+        [Required]
+        public byte AgeRating { get; set; } = 0 ;
+        [StringLength(200)]
         public string Image { get; set; }
+
+        [NotMapped]
         public string HourDuration { get {
             return TimeSpan.FromMinutes(Duration).ToString(@"%h 'h' mm 'min'");
         } }
